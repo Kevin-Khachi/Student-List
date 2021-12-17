d20062c3d97a4a8a18533399645464f3821d51e2
Kevin Khachi <coderkevin500@gmail.com>
2021-12-16 17:40:58 -0800

    "Added a value attribute to the input element 'search bars' and changed 'onKeyDown' attributes to 'onChange'

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,26 +7,20 @@ const MainComponent = () => {
   const [searchTag, setSearchTag] = useState('');
 
   const getName = (event) => {
-    const entry = event.target.value;
-    setName(entry);
-    if (event.code === 'Enter' || event.target.value.length === 0) {
-      event.target.value = '';
-    }
+      const entry = event.target.value;
+      setName(entry);
   }
 
   const getTag = (event) => {
-    const entry = event.target.value;
-    setSearchTag(entry);
-    if (event.code === 'Enter' || event.target.value.length === 0) {
-      event.target.value = '';
-    }
+      const entry = event.target.value;
+      setSearchTag(entry);
   }
 
   return (
     <section className='container'>
       <section id='search-wrapper'>
-        <input id='search-name' onKeyDown={getName} placeholder='Search by name' />
-        <input id='search-tag' onKeyDown={getTag} placeholder='Search by tag' />
+        <input id='search-name' onChange={getName} value={name} placeholder='Search by name' />
+        <input id='search-tag' onChange={getTag} value={searchTag} placeholder='Search by tag' />
       </section>
       <StudentList nameQuery={name} tagQuery={searchTag} />
     </section>
